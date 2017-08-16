c6419baa9c4578020abc236de5068a950b1e0e0a
Kailan Blanks <kailan@kmp.pw>
2017-08-16 22:38:05 +0100

    Take on branding of currently selected team

--- app/models/team.rb
+++ app/models/team.rb
@@ -4,4 +4,8 @@ class Team
   field :name, type: String
   groupify :group, members: :users
   
+  def icon_url
+    # TODO: allow image upload
+    "https://s3-eu-west-1.amazonaws.com/ollycorp/vlt.png"
+  end
 end
